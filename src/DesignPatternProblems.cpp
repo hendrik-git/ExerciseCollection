b421f3f234db95b7e49ce6e4db226ee9b2e32b45
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2023-03-21 22:30:50 +0100

    Added derived classes for implementing the composite pattern

--- src/DesignPatternProblems.cpp
+++ src/DesignPatternProblems.cpp
@@ -150,6 +150,7 @@ TEST_CASE("Validating passwords", "[DesignPatterns]")
 // the generation rules such as the presence of at least one digit, symbol, lower or uppercase
 // character, and so on. These additional rules must be configurable and composable.
 
+/// @brief Interface class for the composite pattern
 class password_generator
 {
   public:
@@ -161,6 +162,118 @@ class password_generator
 	virtual auto add(std::unique_ptr<password_generator> generator) -> void = 0;
 };
 
+class basic_password_generator : public password_generator
+{
+	size_t len_;
+
+  public:
+	explicit basic_password_generator(size_t const len) noexcept : len_(len) {}
+
+	auto generate() -> std::string override
+	{
+		throw std::runtime_error("not implemented");
+	}
+
+	auto add(std::unique_ptr<password_generator> generator) -> void override
+	{
+		throw std::runtime_error("not implemented");
+	}
+
+	auto length() const -> size_t final
+	{
+		return len_;
+	}
+};
+
+class digit_generator : public basic_password_generator
+{
+  public:
+	using basic_password_generator::basic_password_generator;
+
+	auto allowed_chars() const -> std::string override
+	{
+		return "0123456789";
+	}
+};
+
+class symbol_generator : public basic_password_generator
+{
+  public:
+	using basic_password_generator::basic_password_generator;
+
+	auto allowed_chars() const -> std::string override
+	{
+		return "!@#$%^&*(){}[]?<>";
+	}
+};
+
+class upper_letter_generator final : public basic_password_generator
+{
+  public:
+	using basic_password_generator::basic_password_generator;
+
+	auto allowed_chars() const -> std::string override
+	{
+		return "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
+	}
+};
+
+class lower_letter_generator final : public basic_password_generator
+{
+  public:
+	using basic_password_generator::basic_password_generator;
+
+	auto allowed_chars() const -> std::string override
+	{
+		return "abcdefghijklmnopqrstuvxywz";
+	}
+};
+
+class composite_password_generator final : public password_generator
+{
+	[[noreturn]] auto allowed_chars() const -> std::string override
+	{
+		throw std::runtime_error("not implemented");
+	};
+
+	[[noreturn]] auto length() const -> size_t override
+	{
+		throw std::runtime_error("not implemented");
+	};
+
+  public:
+	composite_password_generator()
+	{
+		auto seed_data = std::array<int, std::mt19937::state_size>{};
+		std::ranges::generate(seed_data, std::ref(rdev_));
+		std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+		engine_.seed(seq);
+	}
+
+	auto generate() -> std::string override
+	{
+		std::string password;
+		for(const auto& generator : generators_)
+		{
+			std::string					  chars = generator->allowed_chars();
+			std::uniform_int_distribution udistr(0, static_cast<int>(chars.length() - 1));
+			for(size_t i = 0; i < generator->length(); ++i)
+				password += chars[udistr(engine_)];
+		}
+		std::ranges::shuffle(password, engine_);
+		return password;
+	}
+
+	auto add(std::unique_ptr<password_generator> generator) -> void override
+	{
+		generators_.push_back(std::move(generator));
+	}
+
+  private:
+	std::random_device								 rdev_;
+	std::mt19937									 engine_;
+	std::vector<std::unique_ptr<password_generator>> generators_;
+};
 
 TEST_CASE("Generating passwords", "[DesignPatterns]")
 {
