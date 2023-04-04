73db4e20c64c58e9c84e80d69b2b25896bfefa93
Hendrik Pöttker <hendrik.poettker@web.de>
2023-04-04 13:23:36 +0200

    Added example movies to the unit test for DataSerialization

--- test/DataSerialization.cpp
+++ test/DataSerialization.cpp
@@ -1,4 +1,27 @@
 #include <ExerciseCollection/DataSerialization.hpp>
 #include <catch2/catch_all.hpp>
+#include <iostream>
+#include <ranges>
 
 using namespace DataSerialization;
+
+const movie_list movies{{11001,
+						 "The Matrix",
+						 1999,
+						 196,
+						 {"Lana Wachowski", "Lilly Wachowski"},
+						 {"Lana Wachowski", "Lilly Wachowski"},
+						 {{"Keanu Reeves", "Neo"},
+						  {"Laurence Fishburne", "Morpheus"},
+						  {"Carrie-Anne Moss", "Trinity"},
+						  {"Hugo Weaving", "Agent Smith"}}},
+						{9871,
+						 "Forrest Gump",
+						 1994,
+						 202,
+						 {"Robert Zemeckis"},
+						 {"Winston Groom", "Eric Roth"},
+						 {{"Tom Hanks", "Forrest Gump"},
+						  {"Sally Field", "Mrs. Gump"},
+						  {"Robin Wright", "Jenny Curran"},
+						  {"Mykelti Williamson", "Bubba Blue"}}}};
